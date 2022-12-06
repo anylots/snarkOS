b7f5ef9b9e00f1696e3f78e623e5fb7a87985ac5
anylots <842567494@qq.com>
2022-12-06 23:39:36 +0800

    asfa4

--- node/src/prover/mod.rs
+++ node/src/prover/mod.rs
@@ -214,9 +214,10 @@ impl<N: Network, C: ConsensusStorage<N>> Prover<N, C> {
     ) -> Option<(u64, ProverSolution<N>)> {
         // Increment the puzzle instances.
         self.increment_puzzle_instances();
+        let prover = self.clone();
+        
         prover.solutions_prove.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
 
-        let prover = self.clone();
         let solutions = prover.solutions_prove.load(std::sync::atomic::Ordering::SeqCst);
 
         if solutions > 0 && solutions %10 ==0 {
